31d33f5aa3bb627b298d1bbe6b148bbfce6eedd8
hi-bi <qds315@gmail.com>
2024-06-15 14:39:13 +0300

    feat: change NodejsFunction to lambda.Function

--- cdk/services/serverless/product-service/ProductService.ts
+++ cdk/services/serverless/product-service/ProductService.ts
@@ -1,6 +1,7 @@
 import { Construct } from 'constructs';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+//import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+//import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 
@@ -8,14 +9,27 @@ export class ProductService extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
-        const getProductsList = new NodejsFunction(this, 'getProductsListLambda', {
-            entry: 'services/serverless/product-service/functions/cdk-products.js',
-            runtime: Runtime.NODEJS_LATEST,
+        //const getProductsList = new NodejsFunction(this, 'getProductsListLambda', {
+        //    entry: 'services/serverless/product-service/functions/cdk-products.js',
+        //    runtime: Runtime.NODEJS_LATEST,
+        //});
+
+        // Define the Lambda function resource
+        const getProductsList = new lambda.Function(this, 'getProductsListLambda', {
+            runtime: lambda.Runtime.NODEJS_16_X, // Choose any supported Node.js runtime
+            code: lambda.Code.fromAsset('services/serverless/product-service/functions'), // Points to the lambda directory
+            handler: 'cdk-products.handler', // Points to the 'cdk-products' file in the lambda directory
         });
 
-        const getProduct = new NodejsFunction(this, 'getProductLambda', {
-            entry: 'services/serverless/product-service/functions/cdk-product.js',
-            runtime: Runtime.NODEJS_LATEST,
+        //const getProduct = new NodejsFunction(this, 'getProductLambda', {
+        //    entry: 'services/serverless/product-service/functions/cdk-product.js',
+        //    runtime: Runtime.NODEJS_LATEST,
+        //});
+
+        const getProduct = new lambda.Function(this, 'getProductLambda', {
+            runtime: lambda.Runtime.NODEJS_16_X, // Choose any supported Node.js runtime
+            code: lambda.Code.fromAsset('services/serverless/product-service/functions'), // Points to the lambda directory
+            handler: 'cdk-product.handler', // Points to the 'cdk-products' file in the lambda directory
         });
 
         const api = new apigateway.RestApi(this, 'product-service-api', {
@@ -23,7 +37,7 @@ export class ProductService extends Construct {
         });
 
         const getProductsListPath = api.root.addResource('products'); 
-        // path name https://{createdId}.execute-api.{region}.amazonaws.com/prod/products
+        // path name https://{createdId}.execute-api.us-east.amazonaws.com/prod/products
 
         getProductsListPath.addMethod(
             'GET',
@@ -31,7 +45,7 @@ export class ProductService extends Construct {
         );
 
         const getProductPath = getProductsListPath.addResource('{product_id}');
-        // path name https://{createdId}.execute-api.{region}.amazonaws.com/prod/product-id
+        // path name https://{createdId}.execute-api.us-east.amazonaws.com/prod/{product-id}
 
         getProductPath.addMethod(
             'GET',
