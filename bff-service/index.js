07baea5f12d78c31c636348aca73852dea969c54
hi-bi <qds315@gmail.com>
2024-08-12 00:59:58 +0300

    feat: redirect added

--- bff-service/index.js
+++ bff-service/index.js
@@ -9,11 +9,57 @@ const server = fastify({
 await server.register(cors, { origin: '*' });
 
 server.route({
-    method: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
+    method: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
     url: '/*',
     handler: (req, res) => {
-        return {}
-  }
+        const { originalUrl, method, body: data, headers } = req;
+
+        if (originalUrl === '/favicon.ico') {
+            return res.status(204).send();
+        }
+
+        const [path, queryParams] = req.originalUrl.split('?');
+        const serviceKey = path.split('/')[1];
+
+        const paramsObj = {};
+
+        if (queryParams) {
+          const params = new URLSearchParams(queryParams);
+          for (const [key, value] of params.entries()) {
+            paramsObj[key] = value;
+          }
+        }
+
+        if (serviceKey !== process.env['CART_SERVICE_KEY'] && serviceKey !== process.env['PRODUCT_SERVICE_KEY']) {
+            return res.status(502).send({ error: 'Cannot process request' });
+          }
+
+        const apiHost = process.env[`${serviceKey}_SERVICE_API`.toUpperCase()];
+
+        let url = `${apiHost}/${serviceKey}s`;
+
+        if(paramsObj.id) {
+          url += `/${paramsObj.id}`
+        }
+
+        axios({
+            url,
+            data,
+            method,
+            headers: { 'Authorization': headers.authorization },
+            timeout: 5000,
+            responseType: 'json'
+        }).then((resp) => {
+            const { status, data } = resp;
+
+            res.status(status).send(data);
+        }).catch((e) => {
+            const status = e?.response ? e?.response.status : 500;
+            const data = e?.response ? e?.response.data : 'Internal error ...';
+
+            res.status(status).send(data)
+        });
+    }
 });
 
 try {
